293851f5c90c0220bfbed5176f1ce342ab8593ee
Fernnog <fernandocnogueira@gmail.com>
2025-11-21 14:26:01 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'chess-pwa-v1.0.1'; // Deve bater com config.js se possível, ou atualizado manualmente aqui
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './css/main.css',
+    './src/main.js',
+    './src/modules/ui.js',
+    './src/modules/game.js',
+    './src/modules/engine.js',
+    './src/modules/utils.js',
+    './src/modules/config.js',
+    './src/modules/audio.js',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Força o novo SW a ativar imediatamente
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache); // Limpa cache antigo (v1.0.0)
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
